502ef75aaf9baff0c307365da35cdc69773e8919
Manalu <manaluma@grinnell.edu>
2021-06-16 16:43:53 -0500

    Museum ver 1

--- Assets/Scripts/Newton.cs
+++ Assets/Scripts/Newton.cs
@@ -4,29 +4,57 @@ using UnityEngine;
 
 public class Newton : MonoBehaviour
 {
-    private GameObject player;
-    [SerializeField] 
+    public GameObject Player;
+    // public GameObject TheNPC;
+    // public float TargetDistance;
+    // public float AllowedDistance = 5;
+    public float FollowSpeed;
+
+    // public RaycastHit Shot;
+   
     Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        Player = GameObject.FindGameObjectWithTag("Player");
         animator = GetComponent<Animator>();
     }
 
-    private void OnTriggerEnter(Collider other){
+    // void Update()
+    // {
+    //     transform.LookAt(Player.transform);
 
-       Debug.Log("B");
+    //     if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot)){
+    //         TargetDistance = Shot.distance;
+
+    //         if (TargetDistance >= AllowedDistance){
+    //             FollowSpeed = 0.02f;
+    //             animator.Play("xbot@Walking");
+    //             transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, FollowSpeed);
+    //         }
+    //         else {
+    //             FollowSpeed = 0;
+    //             animator.Play("xbot@Laughing");
+    //         }
+    //     }
+    // }
+    
+    private void OnTriggerEnter(Collider other){
+        transform.LookAt(Player.transform);
         if (other.tag == "Player"){
-            animator.SetBool("isThinking", true);
+            Debug.Log("here");
+            animator.SetBool("isWalking", true);
+            Debug.Log("and here");
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, FollowSpeed);
+            Debug.Log("a here");
         }
     }
 
     private void OnTriggerExit(Collider other){
         Debug.Log("BE");
         if (other.tag == "Player"){
-            animator.SetBool("isThinking", false);
+            animator.SetBool("isWalking", false);
         }
     }
 
