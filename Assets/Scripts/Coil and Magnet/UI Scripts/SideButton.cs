bf65b86222d76e3c276576775b7d0286c1f87f5e
mariomanalu <48722823+mariomanalu@users.noreply.github.com>
2021-07-14 12:19:54 -0500

    Added functional side buttons for the iPed

---
+++ Assets/Scripts/Coil and Magnet/UI Scripts/SideButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SideButton : MonoBehaviour
+{
+    [SerializeField]
+    VectorField field;
+    public void ButtonNext()
+    {
+        field.fieldType = (VectorField.FieldType)((((int)field.fieldType) + 1) % 6);
+    }
+
+    public void ButtonPrev()
+    {
+        field.fieldType = (VectorField.FieldType)((6 + ((int)field.fieldType) - 1) % 6);
+    }
+}
