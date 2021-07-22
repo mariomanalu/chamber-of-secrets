0d0e4c3b58852577c3ec0825e941325e5ac721de
mariomanalu <48722823+mariomanalu@users.noreply.github.com>
2021-07-22 14:58:09 -0500

    Prettified museum

---
+++ Assets/Scripts/Citation.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Citation : MonoBehaviour
+{
+    // "Indoor Plant" (https://skfb.ly/5jhgfd0ba) by 3DHaupt is licensed under Creative Commons Attribution-NonCommercial (http://creativecommons.org/licenses/by-nc/4.0/).
+    // "Flat Screen Television" (https://skfb.ly/69TDG) by abdillaamy is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+}
