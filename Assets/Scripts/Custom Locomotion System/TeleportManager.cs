1a50fdce43c5726db301768c52aa19e5a545cfe3
mariomanalu <48722823+mariomanalu@users.noreply.github.com>
2021-07-27 15:06:00 -0500

    Added two magnet mini scene

--- Assets/Scripts/Custom Locomotion System/TeleportManager.cs
+++ Assets/Scripts/Custom Locomotion System/TeleportManager.cs
@@ -28,25 +28,100 @@ public class TeleportManager : MonoBehaviour
     /// </summary>
     public XRRayInteractor rayInteractor;
 
+    // [SerializeField]
+    // XRRayInteractor UIRayInteractor;
 
+    [SerializeField]
+    TeleportationProvider provider;
 
+    [SerializeField]
+    GameObject reticulePrefab;
+    GameObject reticuleInstance;
+
+    //public bool isUIRayActive = false;
     void Start()
     {
 
         string actionMapName = "XRI RightHand";
     
         teleportAction = actionAsset.FindActionMap(actionMapName).FindAction("Teleport Start");
+        //rayInteractor.enabled = false;
     }
 
     private void Update()
-    {   
-       if (teleportAction.ReadValue<float>() != 0)
+    {  
+        if (teleportAction.ReadValue<float>() != 0)// && !isUIRayActive)
        {
            rayInteractor.enabled = true;
+           
+           teleportDestination destination = CheckLocation();
+            if(destination.validDestination)
+            {
+                // Create and file a teleportation request. 
+                TeleportRequest request = new TeleportRequest()
+                {
+                    destinationPosition = destination.location,
+                };
+                provider.QueueTeleportRequest(request);
+            } 
        }
-       else{
+       else
+       {
            rayInteractor.enabled = false;
        }
+       
+    }
 
+    // public void DeactivateUIRay()
+    // {
+    //     isUIRayActive = false;
+    // }
+
+    // public void ActivateUIRay()
+    // {
+    //     isUIRayActive = true;
+    // }
+    struct teleportDestination
+    {
+        public Vector3 location;
+        public bool validDestination;
+        public Vector3 normal;
+    }
+
+    teleportDestination CheckLocation()
+    {
+        teleportDestination destination = new teleportDestination();
+        destination.validDestination = false;
+
+        // Check that the interactor is enabled and pointing at something. 
+        if (!(rayInteractor.enabled))
+        {
+            return destination;
+        }
+        if (!rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit))
+        {
+            return destination;
+        }
+        // Verify that the user can teleport to the selected location. 
+        TeleportationArea anchor = hit.transform.GetComponent<TeleportationArea>();
+        if (anchor)
+        {
+            destination.validDestination = true;
+            destination.location = anchor.transform.position;
+            //destination.normal = anchor.transform.position.up;
+        }
+        else if (hit.transform.GetComponent<TeleportationArea>())
+        {
+            destination.validDestination = true;
+            destination.location = hit.point;
+            //destination.normal = hit.normal;
+        }
+        else
+        {
+            destination.validDestination = false;
+            
+            return destination;
+        }
+        return destination;
     }
 }
